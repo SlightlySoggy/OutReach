0801f29266ec7897dffd52a6b4e3b16acbfffa31
Maxwell Li <108689192+SlightlySoggy@users.noreply.github.com>
2022-12-17 13:59:02 -0700

    Populating opportunity page

--- Areas/Consoles/Pages/Content/Tools/Opportunities/MyOpportunities/MyOpportunities.cshtml.cs
+++ Areas/Consoles/Pages/Content/Tools/Opportunities/MyOpportunities/MyOpportunities.cshtml.cs
@@ -1,3 +1,5 @@
+using global::Outreach.Pages.Utilities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -5,8 +7,72 @@ namespace Outreach.Areas.Consoles.Pages.Content.Tools.Opportunities.MyOpportunit
 {
     public class MyOpportunitiesModel : PageModel
     {
+
+        public List<Opportunity> oppList { get; set; }
+        public List<Tag> ListTag = new List<Tag>();
+        public string defaultsearchtext = "";
         public void OnGet()
         {
+            GeneralUtilities ut = new GeneralUtilities();
+
+            if (Request.Query["Searchkeyword"] != "")
+            {
+                string txtSearch = Request.Query["Searchkeyword"];
+                oppList = ut.SearchOpportunities(txtSearch, "");
+
+                defaultsearchtext = txtSearch;
+            }
+            else
+            {
+                oppList = ut.GetRecentPostOpportunities(10);
+            }
+
+            Tag tag = new Tag();
+            ListTag = tag.GetReferencedTagsbyOpptunityId(""); // get all active tags
         }
+
+        //public IActionResult OnPostButton1(IFormCollection data)
+        //{
+        //    //...
+        //}
+
+
+        //public IActionResult OnPostButton2(IFormCollection data)
+        //{
+        //    //...
+        //}
+
+        public void OnPost()
+        {
+            // https://www.learnrazorpages.com/razor-pages/handler-methods
+            string clickedbuttonName = "";
+            if (Request.Form["submitbutton1"] != "")
+            {
+                clickedbuttonName = Request.Form["submitbutton1"];
+            }
+            //else if (Request.Form["submitbutton2"] != "")
+            //{ 
+            //    clickedbuttonName = Request.Form["submitbutton2"];
+            //}
+
+
+            string txtSearch = "";
+            if (Request.Form["TxtSearch1"] != "")
+            {
+                txtSearch = Request.Form["TxtSearch1"];
+            }
+            //else if (Request.Form["TxtSearch2"] != "")
+            //{
+            //    txtSearch = Request.Form["TxtSearch2"];
+            //}
+
+            GeneralUtilities ut = new GeneralUtilities();
+
+            if (txtSearch != null || Request.Form["chktag"] != "")
+            {
+                oppList = ut.SearchOpportunities(txtSearch, Request.Form["chktag"]);
+            }
+        }
+        //Response.Redirect("Index");
     }
 }
