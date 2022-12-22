8a1c3af573641c830dd978876c99de6d38cde28f
Maxwell Li <108689192+SlightlySoggy@users.noreply.github.com>
2022-12-21 22:54:35 -0700

    Added Org register form, login with google, and captcha

---
+++ Areas/Identity/Pages/RegisterOrg/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Outreach.Areas.Identity.Pages.RegisterOrg
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
